6bf3a840a4f49915ae3fd08434bf66a8db8bc214
yutatsune <66116053+yutatsune@users.noreply.github.com>
2021-09-10 01:33:10 +0900

    item新規投稿機能の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,21 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
     @items = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    Item.create!(name: item_params[:name], price: item_params[:price], user_id: current_user.id)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price)
   end
 end
